2c4d782609602e836fbe489a6227a362fadd43cb
Jon Falcone <jonfalcone777@gmail.com>
2016-02-18 01:02:25 -0800

    Working on the appointments API (unfinished).

--- modules/appointments/api/appointments.js
+++ modules/appointments/api/appointments.js
@@ -1,1 +1,107 @@
+var express = require('express');
+var moment = require('moment');
+var router = express.Router();
+
+var AppointmentFactory = require('../src/AppointmentFactory');
+var Error = require("../../errors/src/Error");
+var AuthMiddleware = require("../../authentication/src/AuthMiddleware");
+
+// =========================================================================
+// GET - /appointments
+// =========================================================================
+// Get appointments based on query parameters.
+router.get('/', AuthMiddleware.authenticate(), function(req, res) {
+    if (req.query.id) {
+        AppointmentFactory.findAppointmentById(req.query.id, function(err, data) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(data);
+            }
+        });
+    } else {
+        AppointmentFactory.findAppointment({
+            include: req.query.include,
+            exclude: req.query.exclude,
+            paginate: req.query.paginate,
+            perPage: req.query.perPage,
+            page: req.query.page,
+            sortBy: req.query.sortBy,
+            search: req.query.search,
+            status: req.query.status
+        }, function(err, data) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(data);
+            }
+        });
+    }
+});
+
+
+// =========================================================================
+// POST - /appointments
+// =========================================================================
+// Creates a new appointment
+router.post('/', function(req, res) {
+
+    if (!req.body.name) {
+        return res.status(500).send(new Error("APPOINTMENT001"));
+    }
+    var clientId = AppointmentFactory.generateTimeHash(req.body.name);
+
+    ProviderFactory.createProvider({
+        name: req.body.name,
+        clientId: clientId,
+        clientSecret: ProviderFactory.generateTimeHash(clientId),
+        status: req.body.status
+    }, function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+// =========================================================================
+// PUT - /providers
+// =========================================================================
+// Updates a provider
+router.put('/:providerId', function(req, res) {
+
+    ProviderFactory.updateProvider({
+        id: req.params.providerId,
+        name: req.body.name,
+        clientId: req.body.clientId,
+        clientSecret: req.body.clientSecret,
+        status: req.body.status
+    }, function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+// =========================================================================
+// DELETE - /providers
+// =========================================================================
+// deletes a provider
+router.put('/:providerId', function(req, res) {
+    ProviderFactory.deleteProvider(req.params.providerId, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+
+
+module.exports = router;
 /** appointments api goes here **/
--- modules/appointments/src/Appointment.js
+++ modules/appointments/src/Appointment.js
@@ -1,1 +1,109 @@
-/** appointments class goes here **/
+"use strict";
+
+var Provider = require("../../providers/src/Provider"),
+    Visitor = require("../../visitors/src/Visitors");
+
+module.exports = class Appointment {
+
+    constructor(s) {
+
+        //if an id was passed in
+        if (typeof s == "string" || s instanceof String || s._bsontype == "ObjectID") {
+            this.id = String(s);
+            this.isInDatabase = true;
+            this.isPopulated = false;
+        }
+
+        //if an object was passed in
+        else if (s && (typeof s == "object" || s instanceof Object)) {
+            if (s && s._id) {
+                this.id = s._id;
+                this.isInDatabase = true;
+            } else if (s && s.id) {
+                this.id = s.id;
+                this.isInDatabase = true;
+            }
+            if (s && s.provider) {
+                this.provider = s.provider;
+            }
+            if (s && s.visitor) {
+                this.visitor = s.visitor;
+            }
+            if (s && s.start) {
+                this.start = s.start;
+            }
+            if (s && s.end) {
+                this.end = s.end;
+            }
+            if (s && s.status) {
+                this.status = s.status;
+            }
+            this.isPopulated = true;
+        }
+
+        //if nothing was passed in
+        else {
+            this.isInDatabase = false;
+            this.isPopulated = false;
+        }
+    }
+
+    set id(i) {
+        this._id = i;
+    }
+    get id() {
+        return this._id || "";
+    }
+
+    set provider(s) {
+        this._provider = s instanceof Provider ? s : new Provider(s);
+    }
+    get provider() {
+        return this._provider || new Provider();
+    }
+
+    set visitor(s) {
+        this._visitor = s instanceof Visitor ? s : new Visitor(s);
+    }
+    get visitor() {
+        return this._visitor || new Visitor();
+    }
+
+    set start(s){
+        this._start = s;
+    }
+
+    get start(){
+        return this._start || "";
+    }
+
+    set end(s){
+        this._end = s;
+    }
+
+    get end(){
+        return this._end || "";
+    }
+
+    set status(s) {
+        this._status = s;
+    }
+    get status() {
+        return this._status || "";
+    }
+
+    set isPopulated(b) {
+        this._isPopulated = b;
+    }
+    get isPopulated() {
+        return this._isPopulated || false;
+    }
+
+    set isInDatabase(b) {
+        this._isInDatabase = b;
+    }
+    get isInDatabase() {
+        return this._isInDatabase || false;
+    }
+
+};
