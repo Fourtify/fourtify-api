85e274a3865dd0b35a1345f7dd6ade7640ddb692
Amandeep Sarow <asarow@ucsd.edu>
2016-02-18 11:45:49 -0800

    Fleshed out schema with details from back end meeting

--- modules/visitors/schemas/visitors.js
+++ modules/visitors/schemas/visitors.js
@@ -1,1 +1,41 @@
-/** Visitor schema goes here **/
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    relationship = require('mongoose-relationship'),
+    bcrypt = require('bcrypt-nodejs');
+
+var visitorsSchema = mongoose.Schema ({
+    provider: {
+        type: Schema.ObjectId,
+        ref: "providers"
+    },
+    name: {
+        first: String,
+        last: String
+    },
+    email: String,
+    phone: {
+        type: {
+            type: String
+        },
+        number: String
+    },
+    status: String,
+    timeStamp: {
+        created: {
+            type: Date,
+            default: Date.now
+        },
+        updated: {
+            type: Date,
+            default: Date.now
+        }
+    }
+},{
+    id:true,
+    collection: "visitors"
+});
+
+visitorsSchema.pre('save', function(next) {
+    this.timeStamp.updated = new Date();
+    next();
+});
