eacbd61cfff2141c8265dbd91bc722004b10a86d
Amandeep Sarow <asarow@ucsd.edu>
2016-02-18 15:04:10 -0800

    Initial commit with basic skeleton fleshed out. Error codes are missing for now

--- modules/visitors/api/visitors.js
+++ modules/visitors/api/visitors.js
@@ -1,1 +1,131 @@
-/** Visitor api goes here **/
+var express = require('express');
+var router = express.Router();
+var AuthMiddleware = require("../../authentication/src/AuthMiddleware");
+
+var VisitorFactory = require('../src/VisitorFactory');
+var Error = require("../../errors/src/Error");
+
+// =========================================================================
+// GET - /visitor
+// =========================================================================
+// Get visitor based on query parameters.
+router.get('/', AuthMiddleware.authenticate(), function(req, res) {
+    if (req.query.id) {
+        VisitorFactory.findVisitorById({
+            id: req.query.id,
+            provider: req.provider.id
+        }, function(err, data) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(data);
+            }
+        });
+    } else {
+        VisitorFactory.findVisitor({
+            provider: req.provider.id,
+            include: req.query.include,
+            exclude: req.query.exclude,
+            paginate: req.query.paginate,
+            perPage: req.query.perPage,
+            page: req.query.page,
+            sort: req.query.sort,
+            sortBy: req.query.sortBy,
+            search: req.query.search,
+            name: req.query.name,
+            email: req.query.email ? req.query.email.value : undefined,
+            status: req.query.status
+        }, function(err, data) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).send(data);
+            }
+        });
+    }
+});
+
+// =========================================================================
+// POST - /visitor
+// =========================================================================
+// Create a visitor
+router.post('/', AuthMiddleware.authenticate(), function(req, res) {
+
+    if (!req.provider) {
+        return res.status(500).send(new Error("PROVIDER004"));
+    }
+
+    if (!req.body.name) {
+        return res.status(500).send(new Error("??"));
+    }
+
+    VisitorFactory.createVisitor({
+        provider: req.provider.id,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        status: req.body.status
+    }, function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+// =========================================================================
+// PUT - /visitor/:visitorId/profile
+// =========================================================================
+// Update profile elements: basically anything except password
+router.put('/:visitorId/profile', AuthMiddleware.authenticate(), function(req, res) {
+
+    if (!req.provider) {
+        return res.status(500).send(new Error("PROVIDER004"));
+    }
+
+    if (!req.body.name) {
+        return res.status(500).send(new Error("??"));
+    }
+
+    VisitorFactory.updateVisitorProfile({
+        provider: req.provider.id,
+        visitorId: req.params.visitorId,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        status: req.body.status,
+    }, function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+
+// =========================================================================
+// DELETE - /visitor/:visitorId
+// =========================================================================
+// Delete a visitor.
+router.delete('/:visitorId', AuthMiddleware.authenticate(), function(req, res) {
+
+    if (!req.provider) {
+        return res.status(500).send(new Error("PROVIDER004"));
+    }
+
+    VisitorFactory.deleteVisitor({
+        provider: req.provider.id,
+        visitorId: req.params.visitorId
+    }, function(err) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            //@todo transfer future records and write all past records to history
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
